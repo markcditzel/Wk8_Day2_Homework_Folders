96f97043328009fdfe2d8d2363977a1548030555
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-08-14 17:45:33 +0100

    core files and classes (hibernateUtil pom etc) updated; working on Folder class

--- src/main/java/models/Extension.java
+++ src/main/java/models/Extension.java
@@ -1,6 +1,18 @@
 package models;
-
+// this is to enhance the File class - provides a dropdown of file extensions (.jpeg, .txt etc)
 public enum Extension {
+    TXT("txt"),
+    JPEG("jpeg"),
+    MP3("mp3"),
+    MP4("mp4");
 
-    // this is to enhance the File class - provides a dropdown of file extensions (.jpeg, .txt etc)
+    private String extName;
+
+    Extension(String extName){
+        this.extName = extName;
+    }
+
+    public String getExtName(){
+        return extName;
+    }
 }
--- src/main/java/models/Folder.java
+++ src/main/java/models/Folder.java
@@ -1,4 +1,60 @@
 package models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
+
+    private int id;
+    private String name;
+    private Extension extension;
+    private List<File> files;
+
+    public Folder(){
+    }
+
+    public Folder(String name, Extension extension) {
+        this.name = name;
+        this.extension = extension;
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Enumerated(value = EnumType.STRING)
+    Extension getExtension() {
+        return extension;
+    }
+
+    public void setExtension(Extension extension) {
+        this.extension = extension;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
