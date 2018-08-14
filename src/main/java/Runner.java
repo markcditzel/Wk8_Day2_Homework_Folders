d6f91920dbb0f31b111a0e1442ec53081a8a79f3
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-08-14 17:52:56 +0100

    created Enum for file extension type and folder class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,18 @@
+import models.Extension;
+import models.Folder;
+
 public class Runner {
 
     public static void main(String[] args) { // Universal method to allow it to function as a console
+
+        Folder folder = new Folder("Homework", Extension.TXT);
+        Folder Folder = new Folder("Music", Extension.MP3);
+
+
+
+
+
+
     }
 
 
